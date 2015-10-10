99eba226fa30ec8d5c3bd677911f68dc809e1883
nathanael <nathanael.pineau@gmail.com>
2015-10-10 23:21:11 +0200

    dodo

---
+++ lexer.cpp
@@ -0,0 +1,33 @@
+// The lexer return tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+    tok_eof         = -1,
+
+    // commands
+    tok_def         = -2,
+    tok_extern      = -3,
+
+    // primary
+    tok_identifier  = -4,
+    tok_number      = -5
+};
+
+static std::string  IdentifierStr;  // Filled in if tok_identifier
+static double       NumVal;         // Filled in if tok_number
+
+// gettok - Return the next token from standard input.
+static int gettok() {
+    static int LastChar = ' ';
+
+    // Skip any whitspace.
+    while (isspace(LastChar))
+        LastChar = getchar;
+
+    if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+        if
